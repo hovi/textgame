0d960ed9437ea3b2daebc22900c832769d96dc26
Karel Hovorka <info@karel-hovorka.eu>
2018-04-09 08:11:09 +0200

    Initial commit

---
+++ spec/textgame-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import TextgameView from '../lib/textgame-view';
+
+describe('TextgameView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
